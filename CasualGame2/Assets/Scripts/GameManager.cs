846a5d515a199bc51f9e48aef9169ee2bcd22ca0
tylerac13731 <tac7622@g.rit.edu>
2017-04-21 14:29:47 -0400

    Added toggleable quick harvest button

--- CasualGame2/Assets/Scripts/GameManager.cs
+++ CasualGame2/Assets/Scripts/GameManager.cs
@@ -21,9 +21,12 @@ public class GameManager : MonoBehaviour
 	private Vector3 prevMousePosition;
 	public Vector4 limit;
 	
-	private Vector2 distanceTraveled = new Vector2(0, 0);
+	public Vector2 distanceTraveled = new Vector2(0, 0);
 	
 	private bool canMove;
+	private bool quickHarvest = false;
+	
+	private Button quickHarvestButton;
 
 	// Use this for initialization
 	void Start ()
@@ -31,6 +34,8 @@ public class GameManager : MonoBehaviour
         selectedSoul = null;
         soulIsSelected = false;
         selectedImage.SetActive(false);
+		
+		quickHarvestButton = GameObject.Find("GUI").transform.FindChild("QuickHarvest").GetComponent<Button>();
     }
 	
 	// Update is called once per frame
@@ -59,30 +64,24 @@ public class GameManager : MonoBehaviour
 
             selectedImage.SetActive(false);
 
-
-
             if (Physics.Raycast(ray, out hit))
             {
                 if (hit.collider.tag == "Soul")
                 {
-                    SelectSoul(hit.collider.gameObject);
-<<<<<<< HEAD
-                    //if (hit.collider.GetComponent<Soul>().timeToRipe <= 0)
-                    //{
-                    //    playerManager.ChangeEctoplasm(hit.collider.GetComponent<Soul>().ectoPerHarvest);
-                    //    playerManager.ChangeExperience(20);
-                    //}
-                    //hit.collider.GetComponent<Soul>().plot.GetComponent<Plot>().RemoveFromPlot(hit.collider.gameObject);
-=======
-                    if (hit.collider.GetComponent<Soul>().timeToRipe <= 0)
-                    {
-                        playerManager.ChangeEctoplasm(hit.collider.GetComponent<Soul>().ectoPerHarvest);
-                        playerManager.ChangeExperience(20);
-                    }
-                    hit.collider.GetComponent<Soul>().plot.GetComponent<Plot>().RemoveFromPlot(hit.collider.gameObject);
-					hit.collider.GetComponent<Soul>().Harvest();
->>>>>>> 3f707d402a863fc2ad24926c390a2ad7cb350549
-                    //Destroy(hit.collider.gameObject);
+					if(!quickHarvest)
+					{
+						SelectSoul(hit.collider.gameObject);
+					}
+					else
+					{
+						if (hit.collider.GetComponent<Soul>().timeToRipe <= 0)
+						{
+							playerManager.ChangeEctoplasm(hit.collider.GetComponent<Soul>().ectoPerHarvest);
+							playerManager.ChangeExperience(20);
+						}
+						hit.collider.GetComponent<Soul>().plot.GetComponent<Plot>().RemoveFromPlot(hit.collider.gameObject);
+						hit.collider.GetComponent<Soul>().Harvest();
+					}
                 }
                 else
                 {
@@ -119,20 +118,20 @@ public class GameManager : MonoBehaviour
         {
 			if(canMove)
 			{
-				if(distanceTraveled.x + .25f * (Input.mousePosition.x - prevMousePosition.x) < 30 && distanceTraveled.x + .25f * (Input.mousePosition.x - prevMousePosition.x) > -30)
+				if(distanceTraveled.x + (.25f * (prevMousePosition.x - Input.mousePosition.x)) < 30 && distanceTraveled.x + (.25f * (prevMousePosition.x - Input.mousePosition.x)) > -30)
 				{
-					Camera.main.transform.Translate((.25f * (Input.mousePosition.x - prevMousePosition.x) * Mathf.Cos(Mathf.Deg2Rad * -Camera.main.transform.eulerAngles.y)), 0, (.25f * (Input.mousePosition.x - prevMousePosition.x) * Mathf.Sin(Mathf.Deg2Rad * -Camera.main.transform.eulerAngles.y)), Space.World);
-					distanceTraveled.x += .25f * (Input.mousePosition.x - prevMousePosition.x);
+					Camera.main.transform.Translate((.25f * (prevMousePosition.x - Input.mousePosition.x) * Mathf.Cos(Mathf.Deg2Rad * -Camera.main.transform.eulerAngles.y)), 0, (.25f * (prevMousePosition.x - Input.mousePosition.x) * Mathf.Sin(Mathf.Deg2Rad * -Camera.main.transform.eulerAngles.y)), Space.World);
+					distanceTraveled.x += .25f * (prevMousePosition.x - Input.mousePosition.x);
 				}
-				if(distanceTraveled.y + .25f * (Input.mousePosition.y - prevMousePosition.y) < 30 && distanceTraveled.y + .25f * (Input.mousePosition.y - prevMousePosition.y) > -30)
+				if(distanceTraveled.y + (.25f * (prevMousePosition.y - Input.mousePosition.y)) < 30 && distanceTraveled.y + (.25f * (prevMousePosition.y - Input.mousePosition.y)) > -30)
 				{
-					Camera.main.transform.Translate(-(.25f * (Input.mousePosition.y - prevMousePosition.y) * Mathf.Sin(Mathf.Deg2Rad * -Camera.main.transform.eulerAngles.y)), 0, (.25f * (Input.mousePosition.y - prevMousePosition.y) * Mathf.Cos(Mathf.Deg2Rad * -Camera.main.transform.eulerAngles.y)), Space.World);
-					distanceTraveled.y += .25f * (Input.mousePosition.y - prevMousePosition.y);
+					Camera.main.transform.Translate(-(.25f * (prevMousePosition.y - Input.mousePosition.y) * Mathf.Sin(Mathf.Deg2Rad * -Camera.main.transform.eulerAngles.y)), 0, (.25f * (prevMousePosition.y - Input.mousePosition.y) * Mathf.Cos(Mathf.Deg2Rad * -Camera.main.transform.eulerAngles.y)), Space.World);
+					distanceTraveled.y += .25f * (prevMousePosition.y - Input.mousePosition.y);
 				}
 			}
             //combines the previous 2 lines into 1
-            //Camera.main.transform.Translate((.5f * (Input.mousePosition.x - prevMousePosition.x) * Mathf.Cos(Mathf.Deg2Rad * 40)) - (.5f * (Input.mousePosition.y - prevMousePosition.y) * Mathf.Sin(Mathf.Deg2Rad * 40)), 0, (.5f * (Input.mousePosition.y - prevMousePosition.y) * Mathf.Cos(Mathf.Deg2Rad * 40)) + (.5f * (Input.mousePosition.x - prevMousePosition.x) * Mathf.Sin(Mathf.Deg2Rad * 40)), Space.World);
-			if(canMove || Vector2.Distance(Input.mousePosition, prevMousePosition) > 75)
+            //Camera.main.transform.Translate((.5f * (prevMousePosition.x - Input.mousePosition.x) * Mathf.Cos(Mathf.Deg2Rad * 40)) - (.5f * (prevMousePosition.y - Input.mousePosition.y) * Mathf.Sin(Mathf.Deg2Rad * 40)), 0, (.5f * (prevMousePosition.y - Input.mousePosition.y) * Mathf.Cos(Mathf.Deg2Rad * 40)) + (.5f * (prevMousePosition.x - Input.mousePosition.x) * Mathf.Sin(Mathf.Deg2Rad * 40)), Space.World);
+			if(canMove || Vector2.Distance(Input.mousePosition, prevMousePosition) > 10)
 			{
 				prevMousePosition = Input.mousePosition;
 				canMove = true;
@@ -174,7 +173,18 @@ public class GameManager : MonoBehaviour
         soulMenu.transform.GetChild(2).GetComponent<Text>().text = selectedSoul.GetComponent<Soul>().ectoPerHarvest.ToString();
     }
 
-    
+    public void ToggleQuickHarvest()
+	{
+		quickHarvest = !quickHarvest;
+		if(quickHarvest)
+		{
+			quickHarvestButton.gameObject.GetComponent<Image>().color = new Color(208f/255, 200f/255, 251f/255, 1);
+		}
+		else
+		{
+			quickHarvestButton.gameObject.GetComponent<Image>().color = Color.white;
+		}
+	}
 
     /// <summary>
     /// Need an easy way to exit the game to avoid Android doing stupid things
