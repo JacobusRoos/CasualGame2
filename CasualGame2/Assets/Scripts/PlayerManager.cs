6b0c7cba4da79dc71267ff84f641a171ac16d487
Jacob Roos <jxr5789@g.rit.edu>
2017-04-24 14:51:18 -0400

    Ui player info

--- CasualGame2/Assets/Scripts/PlayerManager.cs
+++ CasualGame2/Assets/Scripts/PlayerManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerManager : MonoBehaviour
 {
@@ -14,15 +15,25 @@ public class PlayerManager : MonoBehaviour
     public List<int> levelUnlocks = new List<int>();
     public List<GameObject> levelSouls = new List<GameObject>();
 
+    public GameObject PlayerInfoUI;
+
+    private string[] ectoplasmNotation;
+
     // Use this for initialization
     void Start ()
     {
-        ectoplasm = 50;
+        ectoplasm = 50000;
         level = 1;
         experience = 0;
         nextLevelExperience = 500;
          
         gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+
+        PlayerInfoUI.transform.GetChild(0).GetComponent<Text>().text = "Level " + level;
+
+        PlayerInfoUI.transform.GetChild(1).GetComponent<Text>().text = (int)(ectoplasm) + "";
+
+        ectoplasmNotation = new string[]{ "K", "M", "B", "T", "Qa", "Qi"};
 	}
 	
 	// Update is called once per frame
@@ -45,7 +56,9 @@ public class PlayerManager : MonoBehaviour
         {
             LevelUp();
         }
-	}
+
+        PlayerInfoUI.transform.GetChild(1).GetComponent<Text>().text = GenerateEctoplasmString();
+    }
 
     public void AddPlot(GameObject plot, Vector3 position)
     {
@@ -117,5 +130,35 @@ public class PlayerManager : MonoBehaviour
         {
             gameManager.soulPrefab = levelSouls[levelUnlocks.IndexOf(level)];
         }
+    }
+
+    private string GenerateEctoplasmString()
+    {
+        string ectoplasmString = "";
+
+        int tenFactor = 0;
+
+        int ectoplasmHolder = (int)ectoplasm;
+
+        while(ectoplasmHolder > 1)
+        {
+            ectoplasmHolder /= 10;
+
+            tenFactor++;
+        }
+
+        if(tenFactor <= 4)
+        {
+            ectoplasmString = ((int)ectoplasm).ToString();
+        }
+        else
+        {
+            ectoplasmHolder = (int)ectoplasm / (10 ^ (tenFactor - 3));
+
+
+            ectoplasmString = (float)ectoplasmHolder / 1000 + ectoplasmNotation[tenFactor / 3];
+        }
+
+        return ectoplasmString;
     }
 }
