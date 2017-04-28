84d9eb0d0432285960fc6e68a052a9ae034f8de8
tylerac7622 <tac7622@ad.rit.edu>
2017-04-28 17:17:31 -0400

    Working on getting the souls to properly fade

---
+++ CasualGame2/Assets/Scripts/DeathSoul.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathSoul : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.localScale += new Vector3(0.004f, 0.004f, 0);
+		transform.position += new Vector3(0, 0, 0.02f);
+		Color color;
+		color = transform.GetComponent<SpriteRenderer>().color;
+		color.a -= .02f;
+		transform.GetComponent<SpriteRenderer>().color = color;
+		
+		if(color.a <= 0)
+		{
+			Destroy(gameObject);
+		}
+	}
+}
